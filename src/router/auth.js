934e9d91b564a1402bc766254dfa987ea832df09
문현민 <mhm0627@sju.ac.kr>
2024-08-21 16:29:38 +0900

    login 구현

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,24 +1,58 @@
-import {
-    sejongAuthDelegator,
-    LoginRequestDto,
-} from "@coffee-tree/sejong-auth-delegator";
+import { Router } from "express";
+import { LoginRequestDto,  sejongAuthDelegator } from "@coffee-tree/sejong-auth-delegator";
 import { userInfo } from "../schema/userInfo.js"
 import { strict as assert } from "assert";
 
-const id = userInfo.id; // 테스트
+const AuthRouter = Router();
 
 
+AuthRouter.post('/login', (req, res) => {
+    const userid = new userInfo(req.body);
 
-const login = (userid, password) => {
-    const delegator = sejongAuthDelegator();
-
-    const LoginRequestDto = new LoginRequestDto(userid, password);
-
-    try {
-        const profile = await delegator.getUserProfile(LoginRequestDto);
-        return profile;
-    } catch (error) {
-        console.log("Error fetching user profile", error);
-        throw error;
+    const loginFailedTest  = async () => {
+        const delegator = sejongAuthDelegator();
+    
+        const loginRequestDto = new LoginRequestDto(userid, password);
+    
+        try {
+            const profile = await delegator.getUserProfile(loginRequestDto);
+            return profile;
+    
+            assert.fail("Test Failed: loginFailedTest");
+        } catch (error) {
+            assert.match(
+                error.message,
+                /Failed : login failed/,
+                "Unexpected error message"
+            );
+            console.log("Test passed: loginFailedTest");
+        }
+    };
+    
+    const loginSuccessTest = async (userid, password) => {
+        const delegator = sejongAuthDelegator();
+        const loginRequestDto = new LoginRequestDto(userid, password);
+    
+        try {
+            const profile = await delegator.getUserProfile(loginRequestDto);
+            console.log("Test Passed : loginSuccessTest");
+        } catch (error) {
+            assert.match(
+                error.message,
+                /Failed : login failed/,
+                "Unexpected error message"
+            );
+            assert.fail("Test Failed: loginSuccessTest");
+        }
+    };
+    
+    loginFailedTest();
+    if (process.env.TEST_USER_PASSWORD) {
+        loginSuccessTest();
+    } else {
+        console.log(process.env.TEST_USER_PASSWORD);
+        console.log("export TEST_USER_PASSWORD = 'password'");
     }
-};
+});
+
+module.exports = AuthRouter;
