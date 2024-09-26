b4eaaac682ef362b8e810fc63098ad5203f65776
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2024-09-26 23:39:55 +0900

    login success

--- src/router/auth.js
+++ src/router/auth.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { userInfo } from "../schema/userInfo.js";
 import { LoginRequestDto,  sejongAuthDelegator } from "@coffee-tree/sejong-auth-delegator";
+import { createToken } from "./jwt.js";
 
 const AuthRouter = Router();
 
@@ -13,12 +14,21 @@ AuthRouter.post("/login", async (req, res) => {
     // console.log(loginRequestDto);
     try { // 로그인 성공시
         const profile = await delegator.getUserProfile(loginRequestDto);
-        console.log(profile)
-        if ("내 디비에 이 회원의 정보가 있으면") {
+        console.log(profile);
+        if (userInfo.findOne({id : profile.id})) { // 내 디비에 이 회원의 정보가 있으면
             // 로그인 성공 후 토큰을 넘겨줌
+            const payload = {
+                userid : profile.id,
+            }
+
+            const token = createToken(payload);
+
+            res.cookie('AccessToken', token, { httpOnly: true, maxAge : 1000 * 60 * 60 });   
+            res.redirect('/main');
         }
         else { // 내 디비에 이 회원의 정보가 없으면
-            // major, id만 있는 상태로 디비에 저장
+            // major, id만 있는 상태로 디비에 저장, 이후 register에서 나머지 정보 추가
+            userInfo.insertOne({id: profile.id, major: profile.major});
 
         }
     }
@@ -32,7 +42,14 @@ AuthRouter.post("/register", (req, res) => { // 여기서 토큰 넘겨야 함
     const { name, id, major, phNumber, userType } = req.body;
     try{
         userInfo.findOneAndUpdate({id : id}, {name: name, major: major, phNumber: phNumber, userType: userType});
-        res.status(201).send("User registered successfully");
+        const payload = {
+            userid : id,
+        }
+        const token = createToken(payload);
+
+        res.cookie('AccessToken', token, { httpOnly: true, maxAge : 1000 * 60 * 60 });
+        res.redirect('/main');
+
     } catch (err) {
         res.status(400).send(err);
     }
