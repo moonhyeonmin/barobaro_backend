02618e810d5e641ca364c8e88b0719aeee9fd144
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2024-10-01 17:46:49 +0900

    로그인 동기 설정

--- src/router/auth.js
+++ src/router/auth.js
@@ -29,7 +29,7 @@ AuthRouter.post("/login", async (req, res) => {
         else { // 내 디비에 이 회원의 정보가 없으면
             // major, id만 있는 상태로 디비에 저장, 이후 register에서 나머지 정보 추가
             userInfo.insertOne({id: profile.id, major: profile.major});
-
+            res.status(401).send("회원가입 필요");
         }
     }
     catch (error) { // 로그인 실패시
@@ -38,18 +38,17 @@ AuthRouter.post("/login", async (req, res) => {
     }
 });
 
-AuthRouter.post("/register", (req, res) => { // 여기서 토큰 넘겨야 함
-    const { name, id, major, phNumber, userType } = req.body;
+AuthRouter.post("/register", async (req, res) => { // 여기서 토큰 넘겨야 함 회원가입 api
+    const { name, id, major, phNumber, userType } = req.body; // 정보 받고
     try{
-        userInfo.findOneAndUpdate({id : id}, {name: name, major: major, phNumber: phNumber, userType: userType});
+        await userInfo.findOneAndUpdate({id : id}, {name: name, major: major, phNumber: phNumber, userType: userType});
         const payload = {
             userid : id,
         }
         const token = createToken(payload);
 
         res.cookie('AccessToken', token, { httpOnly: true, maxAge : 1000 * 60 * 60 });
-        res.redirect('/main');
-
+        res.redirect('/main'); // 토큰 발급 끝나면 메인화면으로 넘어감
     } catch (err) {
         res.status(400).send(err);
     }
