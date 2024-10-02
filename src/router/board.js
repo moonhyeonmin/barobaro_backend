e5b6ad35603e8aca4d8595744cbc0c93e34d6697
hyuunminn <132858983+moonhyeonmin@users.noreply.github.com>
2024-10-02 19:00:10 +0900

    board_date, boardSchema 수정 필요

--- src/router/board.js
+++ src/router/board.js
@@ -5,14 +5,22 @@ import { board } from '../schema/board.js';
 const BoardRouter = Router();
 
 BoardRouter.post('/write', async (req, res) => {
-    const Post = req.body; 
+    const Post = {
+        type: req.body.type,
+        userName: req.body.userName,
+        title: req.body.title,
+        content: req.body.content,
+        image: req.body.image,
+        major: req.body.major,
+        board_id: req.body.board_id,
+    } 
 
-    const result = await board.insertOne(Post);
+    const result = await board.insertOne(Post); // insertOne 왜 삽입 안됨?
     res.send(Post.board_id);
 });
 
 BoardRouter.delete('/delete/:boardid', async (req, res) => { // 게시글 삭제
-    const result = await board.deleteOne({ _id: req.params.id });
+    const result = await board.deleteOne({ _id: req.query.boardid });
     
     if (result.deletedCount === 1) {
         res.status(204).send({message: 'Delete Success'});
